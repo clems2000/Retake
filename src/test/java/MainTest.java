236b643526e2a043924b80878e6341b713a3a0cb
wi20b092 <wi20b092@tecnikum-wien.at>
2021-06-30 14:11:01 +0200

    Intellji + Main

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void test_Fuck() {
+        //arrange
+
+        //act
+        String actual = Main.GetMessage();
+        //assert
+        Assertions.assertEquals("Hello World", actual);
+    }
+
+}
